66fa774b328aefb5671da63f4cfa942de76fe41d
Brandon Little <brlittle86@gmail.com>
2017-01-21 20:04:12 -0800

    refactored code to pull data from image objects

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //global variables
 var cycleCount = 0;
 var selectionNumbers = [];
+var previousSelectionNumbers = [];
 var num1 = 0;
 var num2 = 0;
 var num3 = 0;
@@ -10,7 +11,7 @@ var imgEl1, imgEl2, imgEl3;
 var showenImg1, showenImg2, showenImg3;
 var selectEl = document.getElementById('setting-img');
 var chartData = [];
-checkLocal();
+var requiredNumberOfClicks = 20;
 
 //image object constructor
 function ProductImg(imgName, filePath, id){
@@ -21,51 +22,31 @@ function ProductImg(imgName, filePath, id){
   this.timeShown = 0;
 }
 
-//initializing the images into the constructor
-var bag = new ProductImg('bag', 'img/bag.jpg', 'bag');
-var banana = new ProductImg('banana', 'img/banana.jpg', 'banana');
-var bathroom = new ProductImg('bathroom', 'img/bathroom.jpg', 'bathroom');
-var boots = new ProductImg('boots', 'img/boots.jpg', 'boots');
-var breakfast = new ProductImg('breakfast', 'img/breakfast.jpg', 'breakfast');
-var bubblegum = new ProductImg('bubblegum', 'img/bubblegum.jpg', 'bubblegum');
-var chair = new ProductImg('chair', 'img/chair.jpg', 'chair');
-var cthulhu = new ProductImg ('cthulhu', 'img/cthulhu.jpg', 'cthulhu');
-var dogDuck = new ProductImg ('dog-duck', 'img/dog-duck.jpg', 'dogDuck');
-var dragon = new ProductImg ('dragon', 'img/dragon.jpg', 'dragon');
-var pen = new ProductImg ('pen', 'img/pen.jpg', 'pen');
-var petSweep = new ProductImg ('pet-sweep', 'img/pet-sweep.jpg', 'petSweep');
-var scissors = new ProductImg ('scissors', 'img/scissors.jpg', 'scissors');
-var shark = new ProductImg ('shark', 'img/shark.jpg', 'shark');
-var sweep = new ProductImg ('sweep', 'img/sweep.png', 'sweep');
-var tauntaun = new ProductImg ('tauntaun', 'img/tauntaun.jpg', 'tauntaun');
-var unicorn = new ProductImg ('unicorn', 'img/unicorn.jpg', 'unicorn');
-var usb = new ProductImg ('usb', 'img/usb.gif', 'usb');
-var waterCan = new ProductImg ('water-can', 'img/water-can.jpg', 'waterCan');
-var wineGlass = new ProductImg ('wine-glass', 'img/wine-glass.jpg', 'wineGlass');
-
 //pushing the image objects into an array
-var imageArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
+var imageArray = getImageArray();
 
 //function to generate three non-repeating random numbers and store them
 var imgSrc = function() {
-  //chooses a random number, so long as that number was not in the selectionNumbers array and sets it to a given position in the array
-  do {
-    num1 = Math.floor(Math.random() * (imageArray.length + 1));
-  } while (selectionNumbers.includes(num1) || num1 === num2 || num1 === num3 || num1 === 20);
-  do {
-    num2 = Math.floor(Math.random() * (imageArray.length + 1));
-  } while (selectionNumbers.includes(num2) || num2 === num1 || num2 === num3 || num2 === 20);
-  do {
-    num3 = Math.floor(Math.random() * (imageArray.length + 1));
-  } while (selectionNumbers.includes(num3) || num3 === num1 || num3 === num2 || num3 === 20);
+  previousSelectionNumbers = selectionNumbers.slice(0);
 
-  selectionNumbers[0] = num1;
-  selectionNumbers[1] = num2;
-  selectionNumbers[2] = num3;
+  //chooses a random number, so long as that number was not in the selectionNumbers array and sets it to a given position in the array
+  selectionNumbers[0] = getUniqueRandomNumber(previousSelectionNumbers, selectionNumbers);
+  selectionNumbers[1] = getUniqueRandomNumber(previousSelectionNumbers, selectionNumbers);
+  selectionNumbers[2] = getUniqueRandomNumber(previousSelectionNumbers, selectionNumbers);
 
   //log array to confirm non-repeating numbers are generated each time function is run
-  console.log(selectionNumbers);
+  console.log("Current Selected: " + selectionNumbers.toString());
 };
+
+function getUniqueRandomNumber(previous, current) {
+  var number = 0;
+
+  do {
+      number = Math.floor(Math.random() * imageArray.length);
+  } while (previous.includes(number) || current.includes(number));
+
+  return number;
+}
 
 //function to remove the images from the page (currently not in use)
 var imgRem = function(){
@@ -79,13 +60,16 @@ var imgRem = function(){
 var createRendItem = function(){
   var sectionEl = document.createElement('section');
   sectionEl.setAttribute('id', 'funk');
+
   imgEl1 = document.createElement('img');
   imgEl2 = document.createElement('img');
   imgEl3 = document.createElement('img');
   console.log(imgEl1, imgEl2, imgEl3);
+
   imgEl1.setAttribute('id', 'randImg');
   imgEl2.setAttribute('id', 'randImg');
   imgEl3.setAttribute('id', 'randImg');
+
   sectionEl.appendChild(imgEl1);
   sectionEl.appendChild(imgEl2);
   sectionEl.appendChild(imgEl3);
@@ -102,10 +86,11 @@ var rendImg = function (){
   showenImg2 = imageArray[selectionNumbers[1]];
   showenImg3 = imageArray[selectionNumbers[2]];
 
+  // Add back in the timeShown property.
+
   imgEl1.setAttribute('src', imageArray[selectionNumbers[0]].filePath);
   imgEl2.setAttribute('src', imageArray[selectionNumbers[1]].filePath);
   imgEl3.setAttribute('src', imageArray[selectionNumbers[2]].filePath);
-
 };
 
 rendImg();
@@ -131,7 +116,7 @@ function imgThree(){
 var choices = document.getElementById('setting-img');
 choices.addEventListener('click', function clickListener() {
 
-  if (cycleCount < 24) {
+  if (cycleCount < requiredNumberOfClicks) {
     for (var i = 0; i < imageArray.length; i++) {
       if (imageArray[i].imgName == this.imgName) {
         imageArray[i].clickCount++;
@@ -143,17 +128,9 @@ choices.addEventListener('click', function clickListener() {
   } else {
     //build the chart data
     for (var i = 0; i < imageArray.length; i++) {
-      var clickedData = imageArray[i].clickCount;
-      var storedTotal = 0;
-      var storedData = localStorage.getItem(JSON.parse('storedData'));
-      console.log(typeof storedData);
-      for (var j = 0; j < storedData.length; j++) {
-        storedTotal += storedData;
-      }
-      clickedData += storedTotal[i];
-      chartData[i] = clickedData;
+      chartData[i] = imageArray[i].clickCount;
     }
-    localStorage.setItem('storedData', JSON.stringify(chartData));
+    localStorage.setItem('storedData', JSON.stringify(imageArray));
     console.log(chartData);
 
     buildChart();
@@ -165,12 +142,33 @@ choices.addEventListener('click', function clickListener() {
 
 }, false);
 
-//function to prevent clearing localStorage
-function checkLocal() {
-  if (localStorage.storedData) {
-    console.log('Data exists!');
+function getImageArray() {
+  var storedData = localStorage.getItem('storedData');
+  if (storedData !== null) {
+    return JSON.parse(localStorage.getItem('storedData')); // get the item from local storage and return it.
   } else {
-    localStorage.storedData = [];
+    return [
+      new ProductImg('bag', 'img/bag.jpg', 'bag'),
+      new ProductImg('banana', 'img/banana.jpg', 'banana'),
+      new ProductImg('bathroom', 'img/bathroom.jpg', 'bathroom'),
+      new ProductImg('boots', 'img/boots.jpg', 'boots'),
+      new ProductImg('breakfast', 'img/breakfast.jpg', 'breakfast'),
+      new ProductImg('bubblegum', 'img/bubblegum.jpg', 'bubblegum'),
+      new ProductImg('chair', 'img/chair.jpg', 'chair'),
+      new ProductImg ('cthulhu', 'img/cthulhu.jpg', 'cthulhu'),
+      new ProductImg ('dog-duck', 'img/dog-duck.jpg', 'dogDuck'),
+      new ProductImg ('dragon', 'img/dragon.jpg', 'dragon'),
+      new ProductImg ('pen', 'img/pen.jpg', 'pen'),
+      new ProductImg ('pet-sweep', 'img/pet-sweep.jpg', 'petSweep'),
+      new ProductImg ('scissors', 'img/scissors.jpg', 'scissors'),
+      new ProductImg ('tauntaun', 'img/tauntaun.jpg', 'tauntaun'),
+      new ProductImg ('shark', 'img/shark.jpg', 'shark'),
+      new ProductImg ('sweep', 'img/sweep.png', 'sweep'),
+      new ProductImg ('unicorn', 'img/unicorn.jpg', 'unicorn'),
+      new ProductImg ('usb', 'img/usb.gif', 'usb'),
+      new ProductImg ('water-can', 'img/water-can.jpg', 'waterCan'),
+      new ProductImg ('wine-glass', 'img/wine-glass.jpg', 'wineGlass')
+    ];
   }
 }
 
